2fca23d4b95132e2d007df775110795df7486938
Drew Conly <atconly@gmail.com>
2016-07-04 14:10:36 -0600

    init repo & solve first 2 functions

---
+++ functions.js
@@ -0,0 +1,12 @@
+var tripleFive = function() {
+  for (i = 0; i < 3; i++) {
+    console.log("Five!");
+  }
+}
+tripleFive();
+
+var lastLetter = function(string){
+  console.log(string[(string.length - 1)])
+}
+
+lastLetter('island');
