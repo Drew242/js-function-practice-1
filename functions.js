8b2a338a8f54761c31fa1ab6015e873d84bae299
Drew Conly <atconly@gmail.com>
2016-07-04 14:31:29 -0600

    finish functions

--- functions.js
+++ functions.js
@@ -10,3 +10,64 @@ var lastLetter = function(string){
 }
 
 lastLetter('island');
+
+var square = function(n) {
+  console.log(n * n);
+}
+
+square(3);
+
+var negate = function(n) {
+  console.log(-n);
+}
+
+negate(5);
+
+var startsWithA = function(word) {
+  if (word[0] == 'a' || word[0] == 'A') {
+    console.log(true)
+  } else {
+    console.log(false)
+  }
+}
+
+startsWithA('aardvark');
+startsWithA('bumblebee');
+startsWithA('Alligator');
+
+var excite = function(word) {
+  console.log(word + '!!!')
+}
+
+excite('yes');
+
+var sun = function(word) {
+  if (word.indexOf('sun') >= 0) {
+    console.log(true)
+  } else {
+    console.log(false)
+  }
+}
+
+sun('sundial');
+sun('July');
+
+var tiny = function(n) {
+  if (n > 0 && n < 1) {
+    console.log(true)
+  } else {
+    console.log(false)
+  }
+}
+
+tiny(0.5);
+tiny(1.1);
+
+var getSeconds = function(time) {
+  var minutesToSeconds = parseInt(time.split(':')[0]) * 60;
+  var seconds = parseInt(time.split(':')[1]);
+  console.log(minutesToSeconds + seconds)
+}
+
+getSeconds('01:30');
+getSeconds('10:25');
